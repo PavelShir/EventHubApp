2dba1ed2215a15a197ccc7935f24f8b189b22345
Pavel Shirokiy <pshirokiy@gmail.com>
2024-11-29 19:50:28 +0300

    add profile check if Auth and fill userName

--- EventHub/Controllers/MyProfileViewController.swift
+++ EventHub/Controllers/MyProfileViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class MyProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
     
+    private var user: UserModel?
+    
+    var isGuestUser: Bool {
+        return AuthManager.shared.currentUser == nil
+    }
+    
     //    MARK: - UI Elements
     
     let imageSignOutButton = "signout"
@@ -128,6 +134,24 @@ dsgeryertdggwrsrd
         element.translatesAutoresizingMaskIntoConstraints = false
         return element
     }()
+    private lazy var signInButton: UIButton = {
+        var configuration = UIButton.Configuration.filled()
+        configuration.title = "Sign In"
+        configuration.attributedTitle?.font = UIFont(name: "Inter-SemiBold", size: 16)
+        configuration.titleAlignment = .leading
+        configuration.baseForegroundColor = .black
+        
+        configuration.image = UIImage(named: imageSignOutButton)
+        configuration.imagePadding = 16
+        
+        configuration.baseBackgroundColor = .white
+        configuration.contentInsets = NSDirectionalEdgeInsets(top: 13, leading: 19, bottom: 12, trailing: 18)
+        
+        let element = UIButton(configuration: configuration)
+        element.addTarget(self, action: #selector(signInTapped), for: .touchUpInside)
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
     
     private lazy var pictureUser: UIImageView = {
         let element = UIImageView()
@@ -185,6 +209,13 @@ dsgeryertdggwrsrd
         loadProfileData()
         configureUI()
         
+        // check user auth and configure wright button
+        
+        if isGuestUser {
+            setupSignInButton()
+        } else {
+            loadUserData()
+        }
     }
     
     func loadProfileData() {
@@ -201,8 +232,6 @@ dsgeryertdggwrsrd
         if let storedAddress = UserDefaults.standard.string(forKey: "profileAddress") {
             aboutMeText.text = storedAddress
         }
-        
-        
     }
     
     //     MARK: - UI Setup
@@ -259,6 +288,10 @@ dsgeryertdggwrsrd
         loadProfileData()
     }
     
+    @objc func signInTapped(_ sender: UIButton) {
+        let vc = SingInController()
+        navigationController?.pushViewController(vc, animated: true)
+    }
     
     @objc func signOutTapped(_ sender: UIButton) {
             guard
@@ -282,6 +315,16 @@ dsgeryertdggwrsrd
     @objc func tapEditButton() {
         self.navigationController?.pushViewController(EditProfileViewController(), animated: true)
     }
+    
+    func setupSignInButton() {
+        scrollView.addSubview(signInButton)
+        signInButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            signInButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            signInButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -49)
+        ])
+    }
 }
     extension UIImage {
         static func emtyImage(with size: CGSize) -> UIImage? {
@@ -291,3 +334,22 @@ dsgeryertdggwrsrd
             return image
         }
     }
+
+extension MyProfileViewController {
+    private func loadUserData() {
+        guard let user = AuthManager.shared.currentUser else { return }
+        
+        FirestoreManager.shared.getUserData(userID: user.uid) { [weak self] result in
+            switch result {
+            case .success(let userData):
+                if let username = userData["username"] as? String {
+                    self?.nameUser = username
+                    self?.nameLabel.text = username
+                }
+                
+            case .failure(let error):
+                self?.showAlert(title: "Ошибка", message: error.localizedDescription)
+            }
+        }
+    }
+}

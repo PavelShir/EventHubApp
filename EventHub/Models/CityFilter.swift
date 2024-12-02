2c18bc27b670c0e504c38ced2b3e345b6ef8630a
AnnaMelekh <annasmelekhina@gmail.com>
2024-12-02 10:21:16 +0100

    фикс города и категории

--- EventHub/Models/CityFilter.swift
+++ EventHub/Models/CityFilter.swift
@@ -10,18 +10,10 @@ import Foundation
 enum City: String, Codable, CaseIterable {
     case saintPetersburg = "spb"
     case moscow = "msk"
-    case novosibirsk = "nsk"
     case yekaterinburg = "ekb"
     case nizhnyNovgorod = "nnv"
     case kazan = "kzn"
-    case vyborg = "vbg"
-    case samara = "smr"
-    case krasnodar = "krd"
-    case sochi = "sochi"
-    case ufa = "ufa"
-    case krasnoyarsk = "krasnoyarsk"
-    case kyiv = "kev"
-    case newYork = "new-york"
+   
 
     var fullName: String {
             switch self {
@@ -29,31 +21,12 @@ enum City: String, Codable, CaseIterable {
                 return "Saint Petersburg"
             case .moscow:
                 return "Moscow"
-            case .novosibirsk:
-                return "Novosibirsk"
             case .yekaterinburg:
                 return "Yekaterinburg"
             case .nizhnyNovgorod:
                 return "Nizhny Novgorod"
             case .kazan:
                 return "Kazan"
-            case .vyborg:
-                return "Vyborg"
-            case .samara:
-                return "Samara"
-            case .krasnodar:
-                return "Krasnodar"
-            case .sochi:
-                return "Sochi"
-            case .ufa:
-                return "Ufa"
-            case .krasnoyarsk:
-                return "Krasnoyarsk"
-            case .kyiv:
-                return "Kyiv"
-            case .newYork:
-                return "New York"
-            }
         }
 }
 func chooseCity(for city: String) -> City {
@@ -62,32 +35,15 @@ func chooseCity(for city: String) -> City {
         return .saintPetersburg
     case "Moscow":
         return .moscow
-    case "Novosibirsk":
-        return .novosibirsk
     case "Yekaterinburg":
         return .yekaterinburg
     case "Nizhny Novgorod":
         return .nizhnyNovgorod
     case "Kazan":
         return .kazan
-    case "Vyborg":
-        return .vyborg
-    case "Samara":
-        return .samara
-    case "Krasnodar":
-        return .krasnodar
-    case "Sochi":
-        return .sochi
-    case "Ufa":
-        return .ufa
-    case "Krasnoyarsk":
-        return .krasnoyarsk
-    case "Kyiv":
-        return .kyiv
-    case "New York":
-        return .newYork
+    
     default:
-        return .newYork
+        return .moscow
     }
 }
 
